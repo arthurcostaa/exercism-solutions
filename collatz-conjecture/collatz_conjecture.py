d17ff148af2d0d05535406956a17419584fd3b4b
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2024-06-24 11:23:25 -0300

    python - collatz-conjecture

---
+++ collatz-conjecture/collatz_conjecture.py
@@ -0,0 +1,11 @@
+def steps(number):
+    if number < 1:
+        raise ValueError("Only positive integers are allowed")
+
+    counter = 0
+
+    while number != 1:
+        counter += 1
+        number = number // 2 if number % 2 == 0 else 3 * number + 1
+
+    return counter
