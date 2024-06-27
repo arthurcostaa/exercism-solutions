8f504b499124621cbe9bebeb92d8b9bb0bc353b4
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2024-06-27 14:11:28 -0300

    python - isogram (v2)

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,12 +1,3 @@
 def is_isogram(string):
     string = string.replace(' ', '').replace('-', '').lower()
-
-    letters = []
-
-    for char in string:
-        if char not in letters:
-            letters.append(char)
-        else:
-            return False
-
-    return True
+    return len(set(string)) == len(string)
